d67644a447d14f340948c5d3c5d3e61d26628ba8
maxback <36503891+maxback@users.noreply.github.com>
2019-12-16 16:51:36 -0300

    Colcoando o baseURL com https novamente
    estava sem e só funciona com a url do site sem. Porém assim não autoriza a api C#.

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken} from './auth.js';
 
 const api = axios.create({ 
-    baseURL: 'http://app-squad-3-ad-csharp.herokuapp.com/api'
+    baseURL: 'https://app-squad-3-ad-csharp.herokuapp.com/api'
 });
 
 //    baseURL: 'http://localhost:3000/'
